8df81959adf91ff6be4d1a1b8619780031795c90
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-07 19:29:54 +0200

    poprawa

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ let t1 = 'Uwielbiam JavaScript';
 let t2 = 'Jestem świetnym programistą';
 
 
-function displayResults (text1, text2) {
-    
-
-    if (text1.length > text2.length) {
-        console.log(text1);
+function displayResults(text1, text2) {
+        if (text1.length > text2.length) {
+        return(text1);
     } else {
-        console.log(text2);
+        return(text2);
     }
 }
 
-displayResults(t1, t2);
+console.log(1, t2);
